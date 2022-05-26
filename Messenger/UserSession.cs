98f0a948963ab34166e2a9497fc24d64f7cfab81
Madránszki Bence <h048514@stud.u-szeged.hu>
2022-05-26 15:48:21 +0200

    add session vars for storing variables

--- Messenger/UserSession.cs
+++ Messenger/UserSession.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Messenger
 {
     static class UserSession
     {
         public static User LoggedInUser = null;
+        public static Dictionary<string, object> SessionVars = new Dictionary<string, object>();
+        public static List<Form> OpenedWindows = new List<Form>();
     }
 }
