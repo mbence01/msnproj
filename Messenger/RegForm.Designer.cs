d3fba9dad3ad70b584a6f0f699287ca4e15b79cd
Madránszki Bence <h048514@stud.u-szeged.hu>
2022-05-26 13:45:57 +0200

    add regform and reg backend

---
+++ Messenger/RegForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Messenger
+{
+    partial class RegForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegForm));
+            this.signUpText = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textUsername = new System.Windows.Forms.TextBox();
+            this.regBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // signUpText
+            // 
+            resources.ApplyResources(this.signUpText, "signUpText");
+            this.signUpText.Name = "signUpText";
+            this.signUpText.Click += new System.EventHandler(this.signUpText_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // textEmail
+            // 
+            resources.ApplyResources(this.textEmail, "textEmail");
+            this.textEmail.Name = "textEmail";
+            // 
+            // textUsername
+            // 
+            resources.ApplyResources(this.textUsername, "textUsername");
+            this.textUsername.Name = "textUsername";
+            // 
+            // regBtn
+            // 
+            this.regBtn.BackColor = System.Drawing.Color.AliceBlue;
+            this.regBtn.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.regBtn, "regBtn");
+            this.regBtn.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.regBtn.Name = "regBtn";
+            this.regBtn.TabStop = false;
+            this.regBtn.UseVisualStyleBackColor = false;
+            this.regBtn.Click += new System.EventHandler(this.regBtn_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // textPassword
+            // 
+            resources.ApplyResources(this.textPassword, "textPassword");
+            this.textPassword.Name = "textPassword";
+            // 
+            // RegForm
+            // 
+            this.AllowDrop = true;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.signUpText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.textUsername);
+            this.Controls.Add(this.regBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RegForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label signUpText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textUsername;
+        private System.Windows.Forms.Button regBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textPassword;
+    }
+}
---
+++ Messenger/RegForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Messenger
+{
+    public partial class RegForm : Form
+    {
+        private static int USERNAME_MIN_LENGTH = 6;
+        private static int PASSWORD_MIN_LENGTH = 6;
+
+        public RegForm()
+        {
+            InitializeComponent();
+        }
+
+        private void signUpText_Click(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+
+            loginForm.Show();
+            this.Visible = false;
+        }
+
+        private void regBtn_Click(object sender, EventArgs e)
+        {
+            string username = textUsername.Text;
+            string password = textPassword.Text;
+            string email = textEmail.Text;
+
+            if (username.Length == 0)
+            {
+                MessageBox.Show("You should fill in the username text field!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            } else if(username.Length < USERNAME_MIN_LENGTH)
+            {
+                MessageBox.Show("Username should contain minimum " + USERNAME_MIN_LENGTH.ToString() + " characters.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (password.Length == 0)
+            {
+                MessageBox.Show("You should fill in the password field!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            } else if (password.Length < PASSWORD_MIN_LENGTH)
+            {
+                MessageBox.Show("Password should contain minimum " + PASSWORD_MIN_LENGTH.ToString() + " characters.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (email.Length == 0)
+            {
+                MessageBox.Show("You should fill in the e-mail address field!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            List<User> usersWithUsername = User.FindBy(username);
+
+            if(usersWithUsername != null && usersWithUsername.Count > 0)
+            {
+                MessageBox.Show("The given username is already in use. Please select another username.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if(User.IsEmailAddrExists(email))
+            {
+                MessageBox.Show("The given e-mail address is already in use. Please select another e-mail address.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            User createdUser = User.AddNew(username, password, email);
+            
+            if(createdUser == null)
+            {
+                MessageBox.Show("An error has occurred during your registration. Please try again later.", "Register failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            MessageBox.Show("Your registration was successfull. Please log in in order to use this application.", "Register successfull", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Visible = false;
+        }
+    }
+}
