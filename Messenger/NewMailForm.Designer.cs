318288f2dc44b7451339c3cd5c2f299355191411
Madránszki Bence <h048514@stud.u-szeged.hu>
2022-05-26 15:49:50 +0200

    started newmail form, todo: db connection impl

---
+++ Messenger/NewMailForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Messenger
+{
+    partial class NewMailForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textFrom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textTo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textSubject = new System.Windows.Forms.TextBox();
+            this.textBody = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(181, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "New mail";
+            // 
+            // textFrom
+            // 
+            this.textFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textFrom.Location = new System.Drawing.Point(85, 71);
+            this.textFrom.Name = "textFrom";
+            this.textFrom.ReadOnly = true;
+            this.textFrom.Size = new System.Drawing.Size(389, 22);
+            this.textFrom.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(29, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "From:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(48, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "To:";
+            // 
+            // textTo
+            // 
+            this.textTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textTo.Location = new System.Drawing.Point(85, 122);
+            this.textTo.Name = "textTo";
+            this.textTo.Size = new System.Drawing.Size(389, 22);
+            this.textTo.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(12, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Subject:";
+            // 
+            // textSubject
+            // 
+            this.textSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textSubject.Location = new System.Drawing.Point(85, 171);
+            this.textSubject.Name = "textSubject";
+            this.textSubject.Size = new System.Drawing.Size(389, 22);
+            this.textSubject.TabIndex = 5;
+            // 
+            // textBody
+            // 
+            this.textBody.Location = new System.Drawing.Point(16, 215);
+            this.textBody.Multiline = true;
+            this.textBody.Name = "textBody";
+            this.textBody.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBody.Size = new System.Drawing.Size(458, 235);
+            this.textBody.TabIndex = 7;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSend.Location = new System.Drawing.Point(16, 470);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(458, 36);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "SEND E-MAIL";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // NewMailForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 518);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.textBody);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textSubject);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textTo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textFrom);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "NewMailForm";
+            this.Text = "NewMailForm";
+            this.Load += new System.EventHandler(this.NewMailForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textFrom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textTo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textSubject;
+        private System.Windows.Forms.TextBox textBody;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
---
+++ Messenger/NewMailForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Messenger
+{
+    public partial class NewMailForm : Form
+    {
+        public NewMailForm()
+        {
+            InitializeComponent();
+            this.FormClosing += NewMailForm_Close;
+        }
+
+        private void NewMailForm_Close(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Program.newMailForm = new NewMailForm();
+        }
+
+        private void NewMailForm_Load(object sender, EventArgs e)
+        {
+            textFrom.Text = UserSession.LoggedInUser.Email;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string fromText = textFrom.Text;
+            string toText = textTo.Text;
+            string subjectText = textSubject.Text;
+            string bodyText = textBody.Text;
+            object reply;
+            
+            UserSession.SessionVars.TryGetValue("MailReply", out reply);
+
+            if(fromText == null || fromText.Length == 0 || toText == null || toText.Length == 0)
+            {
+                MessageBox.Show("You should fill in all the e-mail address fields.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if(bodyText == null || bodyText.Length == 0)
+            {
+                MessageBox.Show("You should write something in the message field!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if(!UserSession.LoggedInUser.Email.Equals(fromText))
+            {
+                MessageBox.Show("You should NOT edit your e-mail address!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if(!User.IsEmailAddrExists(toText))
+            {
+                MessageBox.Show("The given e-mail address is not registered. Check if you mistyped the address.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // TODO: Insert the new mail to the db
+        }
+    }
+}
