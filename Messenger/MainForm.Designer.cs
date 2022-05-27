031ef48ebe07067bf17dc035f65cfed085bb3a32
Madránszki Bence <h048514@stud.u-szeged.hu>
2022-05-27 15:54:46 +0200

    add incoming mail list to main view

--- Messenger/MainForm.Designer.cs
+++ Messenger/MainForm.Designer.cs
@@ -34,6 +34,13 @@
             this.myProfileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.logOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mailList = new System.Windows.Forms.ListView();
+            this.columnHeader0 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,8 +68,9 @@
             // myMailsToolStripMenuItem
             // 
             this.myMailsToolStripMenuItem.Name = "myMailsToolStripMenuItem";
-            this.myMailsToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
-            this.myMailsToolStripMenuItem.Text = "My mails";
+            this.myMailsToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.myMailsToolStripMenuItem.Text = "Sent mails";
+            this.myMailsToolStripMenuItem.Click += new System.EventHandler(this.myMailsToolStripMenuItem_Click);
             // 
             // myProfileToolStripMenuItem
             // 
@@ -84,11 +92,66 @@
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
+            // mailList
+            // 
+            this.mailList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mailList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader0,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.mailList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mailList.HideSelection = false;
+            this.mailList.Location = new System.Drawing.Point(0, 24);
+            this.mailList.Name = "mailList";
+            this.mailList.Size = new System.Drawing.Size(800, 426);
+            this.mailList.TabIndex = 1;
+            this.mailList.UseCompatibleStateImageBehavior = false;
+            this.mailList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader0
+            // 
+            this.columnHeader0.Text = "ID";
+            this.columnHeader0.Width = 25;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "From";
+            this.columnHeader1.Width = 35;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Subject";
+            this.columnHeader2.Width = 48;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Content";
+            this.columnHeader3.Width = 49;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Date";
+            this.columnHeader4.Width = 643;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(506, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TODO: Olvasott, olvasatlan e-mailek + table row onclick -> email megnyitás, válas" +
+    "z lehetőség, e-mail törlés";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mailList);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
@@ -109,5 +172,12 @@
         private System.Windows.Forms.ToolStripMenuItem myProfileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem logOutToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ListView mailList;
+        private System.Windows.Forms.ColumnHeader columnHeader0;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Messenger/MainForm.cs
+++ Messenger/MainForm.cs
@@ -18,6 +18,23 @@ namespace Messenger
             this.FormClosing += Main_Closing;
         }
 
+        private void UpdateList()
+        {
+            List<Mail> mails = Mail.FindBy(UserSession.LoggedInUser.Id);
+
+            foreach (Mail mail in mails)
+            {
+                ListViewItem mailItem = new ListViewItem(mail.Id.ToString());
+
+                mailItem.SubItems.Add(mail.Sender.Email);
+                mailItem.SubItems.Add(mail.MailSubject);
+                mailItem.SubItems.Add(mail.MailBody.Substring(0, Math.Min(30, mail.MailBody.Length)) + "...");
+                mailItem.SubItems.Add(mail.MailDate.ToString());
+
+                mailList.Items.Add(mailItem);
+            }
+        }
+
         private void Main_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             Application.Exit();
@@ -31,6 +48,7 @@ namespace Messenger
 
                 Program.ChangeForm(this, new LoginForm());
             }
+            UpdateList();
         }
 
         private void homeToolStripMenuItem_Click(object sender, EventArgs e)
@@ -40,6 +58,8 @@ namespace Messenger
 
         private void logOutToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            UserSession.LoggedInUser.AddUserLog(User.LOGIN_TYPE_LOGOUT);
+
             UserSession.LoggedInUser = null;
 
             Program.ChangeForm(this, Program.loginForm);
@@ -47,12 +67,21 @@ namespace Messenger
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            if(UserSession.LoggedInUser != null)
+                UserSession.LoggedInUser.AddUserLog(User.LOGIN_TYPE_LOGOUT);
+
             Environment.Exit(0);
         }
 
         private void newMailToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            UserSession.SessionVars["MailReply"] = 0;
             Program.ChangeForm(this, Program.newMailForm, false);
+        }
+
+        private void myMailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Program.ChangeForm(this, Program.mailListForm, false);
         }
     }
 }
