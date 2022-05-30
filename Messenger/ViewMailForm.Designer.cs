4293f4ef96109579b93a26bb68f1cdd5ec67c8aa
Madránszki Bence <h048514@stud.u-szeged.hu>
2022-05-30 09:29:26 +0200

    add new form for viewing email's content

---
+++ Messenger/ViewMailForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Messenger
+{
+    partial class ViewMailForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msgFromText = new System.Windows.Forms.Label();
+            this.dateText = new System.Windows.Forms.Label();
+            this.replyBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.markAsUnreadBtn = new System.Windows.Forms.Button();
+            this.subjectText = new System.Windows.Forms.Label();
+            this.mailContentText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // msgFromText
+            // 
+            this.msgFromText.AutoSize = true;
+            this.msgFromText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.msgFromText.Location = new System.Drawing.Point(95, 9);
+            this.msgFromText.Name = "msgFromText";
+            this.msgFromText.Size = new System.Drawing.Size(298, 25);
+            this.msgFromText.TabIndex = 0;
+            this.msgFromText.Text = "Message from asd@asd.hu";
+            // 
+            // dateText
+            // 
+            this.dateText.AutoSize = true;
+            this.dateText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateText.Location = new System.Drawing.Point(364, 463);
+            this.dateText.Name = "dateText";
+            this.dateText.Size = new System.Drawing.Size(129, 16);
+            this.dateText.TabIndex = 1;
+            this.dateText.Text = "2022. 05. 30. 11:14:10";
+            // 
+            // replyBtn
+            // 
+            this.replyBtn.Location = new System.Drawing.Point(143, 460);
+            this.replyBtn.Name = "replyBtn";
+            this.replyBtn.Size = new System.Drawing.Size(75, 23);
+            this.replyBtn.TabIndex = 2;
+            this.replyBtn.Text = "Reply";
+            this.replyBtn.UseVisualStyleBackColor = true;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(249, 460);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteBtn.TabIndex = 3;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // markAsUnreadBtn
+            // 
+            this.markAsUnreadBtn.Location = new System.Drawing.Point(12, 460);
+            this.markAsUnreadBtn.Name = "markAsUnreadBtn";
+            this.markAsUnreadBtn.Size = new System.Drawing.Size(104, 23);
+            this.markAsUnreadBtn.TabIndex = 4;
+            this.markAsUnreadBtn.Text = "Mark as unread";
+            this.markAsUnreadBtn.UseVisualStyleBackColor = true;
+            this.markAsUnreadBtn.Click += new System.EventHandler(this.markAsUnreadBtn_Click);
+            // 
+            // subjectText
+            // 
+            this.subjectText.AutoSize = true;
+            this.subjectText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.subjectText.Location = new System.Drawing.Point(8, 60);
+            this.subjectText.Name = "subjectText";
+            this.subjectText.Size = new System.Drawing.Size(157, 20);
+            this.subjectText.TabIndex = 5;
+            this.subjectText.Text = "Subject: Test subject";
+            // 
+            // mailContentText
+            // 
+            this.mailContentText.Location = new System.Drawing.Point(12, 106);
+            this.mailContentText.Multiline = true;
+            this.mailContentText.Name = "mailContentText";
+            this.mailContentText.ReadOnly = true;
+            this.mailContentText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.mailContentText.Size = new System.Drawing.Size(481, 331);
+            this.mailContentText.TabIndex = 6;
+            // 
+            // ViewMailForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 488);
+            this.Controls.Add(this.mailContentText);
+            this.Controls.Add(this.subjectText);
+            this.Controls.Add(this.markAsUnreadBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.replyBtn);
+            this.Controls.Add(this.dateText);
+            this.Controls.Add(this.msgFromText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "ViewMailForm";
+            this.Text = "ViewMail";
+            this.Load += new System.EventHandler(this.ViewMailForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label msgFromText;
+        private System.Windows.Forms.Label dateText;
+        private System.Windows.Forms.Button replyBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button markAsUnreadBtn;
+        private System.Windows.Forms.Label subjectText;
+        private System.Windows.Forms.TextBox mailContentText;
+    }
+}
---
+++ Messenger/ViewMailForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Messenger
+{
+    public partial class ViewMailForm : Form
+    {
+        private static string MSG_FROM_TEXT_PREFIX = "Message from ";
+        private static string SUBJECT_TEXT_PREFIX = "Subject: ";
+
+        private Mail currMail;
+
+        public ViewMailForm()
+        {
+            InitializeComponent();
+            this.FormClosing += ViewMailForm_Close;
+        }
+
+        private void ViewMailForm_Load(object sender, EventArgs e)
+        {
+            currMail = (Mail)UserSession.SessionVars["CurrentMail"];
+
+            if (currMail == null)
+                this.Close();
+
+            msgFromText.Text = MSG_FROM_TEXT_PREFIX + currMail.Sender.Email;
+            subjectText.Text = SUBJECT_TEXT_PREFIX + currMail.MailSubject;
+            mailContentText.Text = currMail.MailBody;
+            dateText.Text = currMail.MailDate.ToString();
+        }
+        private void ViewMailForm_Close(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Program.viewMailForm = new ViewMailForm();
+        }
+
+        private void markAsUnreadBtn_Click(object sender, EventArgs e)
+        {
+            if (currMail == null)
+                return;
+
+            currMail.Read = 0;
+            currMail.Update();
+
+            MessageBox.Show("This message is now unread.", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (currMail == null)
+                return;
+
+            DialogResult res = MessageBox.Show("Are you sure you want to delete this e-mail?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            Console.WriteLine(res.ToString());
+        }
+    }
+}
